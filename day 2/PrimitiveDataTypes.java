3e78054bf8188186cc81c6a438e74c57fae31dea
tanosramos <145928026+tanosramos@users.noreply.github.com>
2023-09-24 12:09:31 -0600

    Add files via upload

---
+++ day 2/PrimitiveDataTypes.java
@@ -0,0 +1,24 @@
+public class PrimitiveDataTypes {
+    public static void main(String[] args) {
+        char myChar = 'A';
+        byte myByte = 127;
+        short myShort = 1234;
+        int myInt = 32;
+        long myLong = 112222222222l;
+        boolean myBool = true;
+        String myString = "Jim";
+        double myDouble = 3.2;
+        Float myFloat = 3.22f;
+
+        System.out.println(myChar);
+        System.out.println(myByte);
+        System.out.println(myShort);
+        System.out.println(myInt);
+        System.out.println(myBool);
+        System.out.println(myLong);
+        System.out.println(myString);
+        System.out.println(myDouble);
+        System.out.println(myFloat);
+
+    }
+}
